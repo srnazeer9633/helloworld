30647040927554afbf1124fc7ccdb56c90713637
srnazeer <srnazeer@myseneca.ca>
2022-09-15 17:54:06 -0700

    second

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path= require("path");
 app.get("/",function(req,res)
 {
     console.log(__dirname);
-    res.send("home page");
+    res.send("Subin Raj Nazeer 123044216");
     //res.sendFile(path.join(__dirname,"home.html"));
 });
 var port=process.env.PORT || 3000;
